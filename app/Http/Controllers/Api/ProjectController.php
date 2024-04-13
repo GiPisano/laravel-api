d4c570b6641747539944fc6857a76ca71f920f57
GiPisano <pisanogiacomo9@gmail.com>
2024-04-13 11:16:30 +0200

    add api & api/projectController

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     */
+    public function index()
+    {
+        $projects = Project::select('id', 'user_id', 'type_id', 'title', 'description', 'image')
+            ->with(['type:id,label,color', 'technologies:id,label,color'])
+            ->paginate(10);
+        return response()->json($projects);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     */
+    public function show($id)
+    {
+        //
+    }
+}
